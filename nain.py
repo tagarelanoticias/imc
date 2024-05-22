a0021ef61e522a85e976c2939a52ef8a30ef497f
tagarelanoticias <otagarela.noticiaspcd@gmail.com>
2024-05-21 21:11:50 -0300

    timer

--- nain.py
+++ nain.py
@@ -1,6 +1,13 @@
+import os
+import time
+
 # contador
 
 contador = int(input('informe um numero inteiro:')) 
+os.system('cls')
+
 while contador >= 0:
-    print(contador)
+    print(f'contagem regressiva:{contador}')
+    time.sleep(1)
+    os.system('cls')
     contador -= 1
